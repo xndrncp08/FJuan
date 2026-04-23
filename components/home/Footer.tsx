9a5029ea5e8cfa5b24fec0fd1e6400938e939125
xder-08 <xander.rancap@edu.sait.ca>
2026-04-22 22:48:44 -0600

    fix footer author

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => (
 
           {/* Author credit */}
           <p className="data-readout" style={{ fontSize: "0.68rem" }}>
-            Built by Xander Ranc
+            Independently built by Xander Ranc
           </p>
         </div>
 
